3af0e07298b7fdd4139e6eebf756a0e8e3df88ff
Hindawi44 <409hindwi@gmail.com>
2026-07-16 20:34:30 +0200

    Export financial intelligence API

--- src/opportunity_engine/ods/__init__.py
+++ src/opportunity_engine/ods/__init__.py
@@ -11,6 +11,7 @@ from .confidence import (
 from .discovery import FashionDiscoveryPlugin
 from .discovery_framework import CuratedDiscoveryPlugin, OpportunitySeed, Scanner
 from .evidence_weighting import EvidenceAdjustment, calculate_ssb_adjustment
+from .financial import FinancialInputs, FinancialReport, FinancialScenario, build_financial_report
 from .live_data import (
     DEFAULT_EXTRACTION_RULES,
     DataConnector,
@@ -48,16 +49,17 @@ __all__ = [
     "BrregConnector", "BrregEvidenceSummary", "BusinessBlueprint",
     "CuratedDiscoveryPlugin", "DEFAULT_EXTRACTION_RULES", "DEFAULT_WORKFLOW",
     "DataConnector", "EvidenceAdjustment", "ExtractionRule", "FashionDiscoveryPlugin",
-    "LiveDataPipeline", "LiveDataResult", "ODSAnalysisResult", "ODSPlugin",
-    "ODSRequest", "ODSSession", "OpportunityCandidate", "OpportunityConfidence",
-    "OpportunityExtractor", "OpportunityRankingPlugin", "OpportunitySeed",
-    "PluginRegistry", "RankedOpportunity", "RankingWeights", "SSB_API_BASE",
-    "SSB_RETAIL_TABLE_ID", "SSB_RETAIL_TABLE_URL", "SSBClient", "SSBConnector",
-    "SSBMarketEvidence", "SSBMarketEvidenceService", "SSBTrendIntelligenceService",
-    "SSBTrendSignal", "Scanner", "SourceDocument", "Stage", "StageResult",
-    "StaticDataConnector", "Status", "TrendAdjustment", "USABLE_ALPHA_WORKFLOW",
-    "ValidationExperiment", "ValidationPlugin", "ValidationReport", "WorkflowEngine",
-    "analyze_json_stat2", "analyze_series", "build_alpha_engine",
-    "calculate_opportunity_confidence", "calculate_ssb_adjustment",
-    "calculate_trend_adjustment", "run_ods", "summarize_brreg_entities",
+    "FinancialInputs", "FinancialReport", "FinancialScenario", "LiveDataPipeline",
+    "LiveDataResult", "ODSAnalysisResult", "ODSPlugin", "ODSRequest", "ODSSession",
+    "OpportunityCandidate", "OpportunityConfidence", "OpportunityExtractor",
+    "OpportunityRankingPlugin", "OpportunitySeed", "PluginRegistry", "RankedOpportunity",
+    "RankingWeights", "SSB_API_BASE", "SSB_RETAIL_TABLE_ID", "SSB_RETAIL_TABLE_URL",
+    "SSBClient", "SSBConnector", "SSBMarketEvidence", "SSBMarketEvidenceService",
+    "SSBTrendIntelligenceService", "SSBTrendSignal", "Scanner", "SourceDocument",
+    "Stage", "StageResult", "StaticDataConnector", "Status", "TrendAdjustment",
+    "USABLE_ALPHA_WORKFLOW", "ValidationExperiment", "ValidationPlugin",
+    "ValidationReport", "WorkflowEngine", "analyze_json_stat2", "analyze_series",
+    "build_alpha_engine", "build_financial_report", "calculate_opportunity_confidence",
+    "calculate_ssb_adjustment", "calculate_trend_adjustment", "run_ods",
+    "summarize_brreg_entities",
 ]
