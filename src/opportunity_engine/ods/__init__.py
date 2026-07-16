cbec50381af83a45208083016c778b809ff76d05
Hindawi44 <409hindwi@gmail.com>
2026-07-16 11:08:22 +0200

    Expose discovery framework API

--- src/opportunity_engine/ods/__init__.py
+++ src/opportunity_engine/ods/__init__.py
@@ -1,6 +1,7 @@
 """Opportunity Development System core package."""
 
 from .discovery import FashionDiscoveryPlugin
+from .discovery_framework import CuratedDiscoveryPlugin, OpportunitySeed, Scanner
 from .models import (
     ODSRequest,
     ODSSession,
@@ -13,13 +14,16 @@ from .plugins import ODSPlugin, PluginRegistry
 from .workflow import DEFAULT_WORKFLOW, WorkflowEngine
 
 __all__ = [
+    "CuratedDiscoveryPlugin",
     "DEFAULT_WORKFLOW",
     "FashionDiscoveryPlugin",
     "ODSPlugin",
     "ODSRequest",
     "ODSSession",
     "OpportunityCandidate",
+    "OpportunitySeed",
     "PluginRegistry",
+    "Scanner",
     "Stage",
     "StageResult",
     "Status",
