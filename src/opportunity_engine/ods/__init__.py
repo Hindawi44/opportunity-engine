b85f4655f8877717533a87ff241793aab049bd1e
Hindawi44 <409hindwi@gmail.com>
2026-07-16 19:55:58 +0200

    Export SSB evidence weighting API

--- src/opportunity_engine/ods/__init__.py
+++ src/opportunity_engine/ods/__init__.py
@@ -3,6 +3,7 @@
 from .bdna import BDNAPlugin, BusinessBlueprint
 from .discovery import FashionDiscoveryPlugin
 from .discovery_framework import CuratedDiscoveryPlugin, OpportunitySeed, Scanner
+from .evidence_weighting import EvidenceAdjustment, calculate_ssb_adjustment
 from .live_data import (
     DEFAULT_EXTRACTION_RULES,
     DataConnector,
@@ -13,14 +14,7 @@ from .live_data import (
     SourceDocument,
     StaticDataConnector,
 )
-from .models import (
-    ODSRequest,
-    ODSSession,
-    OpportunityCandidate,
-    Stage,
-    StageResult,
-    Status,
-)
+from .models import ODSRequest, ODSSession, OpportunityCandidate, Stage, StageResult, Status
 from .plugins import ODSPlugin, PluginRegistry
 from .ranking import OpportunityRankingPlugin, RankedOpportunity, RankingWeights
 from .runner import ODSAnalysisResult, USABLE_ALPHA_WORKFLOW, build_alpha_engine, run_ods
@@ -35,45 +29,17 @@ from .validation import ValidationExperiment, ValidationPlugin, ValidationReport
 from .workflow import DEFAULT_WORKFLOW, WorkflowEngine
 
 __all__ = [
-    "BDNAPlugin",
-    "BusinessBlueprint",
-    "CuratedDiscoveryPlugin",
-    "DEFAULT_EXTRACTION_RULES",
-    "DEFAULT_WORKFLOW",
-    "DataConnector",
-    "ExtractionRule",
-    "FashionDiscoveryPlugin",
-    "LiveDataPipeline",
-    "LiveDataResult",
-    "ODSAnalysisResult",
-    "ODSPlugin",
-    "ODSRequest",
-    "ODSSession",
-    "OpportunityCandidate",
-    "OpportunityExtractor",
-    "OpportunityRankingPlugin",
-    "OpportunitySeed",
-    "PluginRegistry",
-    "RankedOpportunity",
-    "RankingWeights",
-    "SSB_API_BASE",
-    "SSB_RETAIL_TABLE_ID",
-    "SSB_RETAIL_TABLE_URL",
-    "SSBClient",
-    "SSBConnector",
-    "SSBMarketEvidence",
-    "SSBMarketEvidenceService",
-    "Scanner",
-    "SourceDocument",
-    "Stage",
-    "StageResult",
-    "StaticDataConnector",
-    "Status",
-    "USABLE_ALPHA_WORKFLOW",
-    "ValidationExperiment",
-    "ValidationPlugin",
-    "ValidationReport",
-    "WorkflowEngine",
-    "build_alpha_engine",
-    "run_ods",
+    "BDNAPlugin", "BusinessBlueprint", "CuratedDiscoveryPlugin",
+    "DEFAULT_EXTRACTION_RULES", "DEFAULT_WORKFLOW", "DataConnector",
+    "EvidenceAdjustment", "ExtractionRule", "FashionDiscoveryPlugin",
+    "LiveDataPipeline", "LiveDataResult", "ODSAnalysisResult", "ODSPlugin",
+    "ODSRequest", "ODSSession", "OpportunityCandidate", "OpportunityExtractor",
+    "OpportunityRankingPlugin", "OpportunitySeed", "PluginRegistry",
+    "RankedOpportunity", "RankingWeights", "SSB_API_BASE",
+    "SSB_RETAIL_TABLE_ID", "SSB_RETAIL_TABLE_URL", "SSBClient", "SSBConnector",
+    "SSBMarketEvidence", "SSBMarketEvidenceService", "Scanner", "SourceDocument",
+    "Stage", "StageResult", "StaticDataConnector", "Status",
+    "USABLE_ALPHA_WORKFLOW", "ValidationExperiment", "ValidationPlugin",
+    "ValidationReport", "WorkflowEngine", "build_alpha_engine",
+    "calculate_ssb_adjustment", "run_ods",
 ]
