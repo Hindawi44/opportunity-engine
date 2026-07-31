3193f0cfb038da8136cca8c279c8b132080123c7
Hindawi44 <409hindwi@gmail.com>
2026-07-31 14:22:58 +0200

    Export shipment evidence workflow

--- src/opportunity_engine/logistics/__init__.py
+++ src/opportunity_engine/logistics/__init__.py
@@ -4,6 +4,12 @@ from .operational_transport import (
     EXPORT_SCHEMA_VERSION as OPERATIONAL_TRANSPORT_EXPORT_SCHEMA_VERSION,
     build_operational_transport_export,
 )
+from .shipment_evidence import (
+    SCHEMA_VERSION as SHIPMENT_EVIDENCE_SCHEMA_VERSION,
+    ShipmentEvidenceError,
+    ShipmentEvidenceTaskV1,
+    build_shipment_evidence_queue,
+)
 from .transport_estimate import (
     TransportEstimateError,
     TransportEstimateInputV1,
@@ -13,9 +19,13 @@ from .transport_estimate import (
 
 __all__ = [
     "OPERATIONAL_TRANSPORT_EXPORT_SCHEMA_VERSION",
+    "SHIPMENT_EVIDENCE_SCHEMA_VERSION",
+    "ShipmentEvidenceError",
+    "ShipmentEvidenceTaskV1",
     "TransportEstimateError",
     "TransportEstimateInputV1",
     "TransportQuoteV1",
     "build_operational_transport_export",
+    "build_shipment_evidence_queue",
     "build_transport_estimate_snapshot",
 ]
