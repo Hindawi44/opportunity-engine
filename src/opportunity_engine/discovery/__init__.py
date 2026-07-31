0a0905d80ab7c4575e5ee2d612cefa3e58065907
Hindawi44 <409hindwi@gmail.com>
2026-07-31 18:42:53 +0200

    Export Norway textile keyword pack

--- src/opportunity_engine/discovery/__init__.py
+++ src/opportunity_engine/discovery/__init__.py
@@ -11,6 +11,12 @@ from opportunity_engine.discovery.e2e_checkpoint import (
     run_controlled_clothing_inventory_checkpoint,
 )
 from opportunity_engine.discovery.models import DiscoveryCandidate, DiscoveryResult
+from opportunity_engine.discovery.norway_textile_keywords import (
+    DOMAIN as NORWAY_TEXTILE_KEYWORD_DOMAIN,
+    SCHEMA_VERSION as NORWAY_TEXTILE_KEYWORD_SCHEMA_VERSION,
+    NorwayTextileKeywordQuery,
+    build_norway_textile_keyword_queries,
+)
 from opportunity_engine.discovery.opportunity_maps import CLOTHING_INVENTORY_MAP
 from opportunity_engine.discovery.query_builder import build_clothing_inventory_queries
 from opportunity_engine.discovery.textile_taxonomy import (
@@ -32,6 +38,9 @@ __all__ = [
     "DiscoveryCandidate",
     "DiscoveryResult",
     "EligibilityDecision",
+    "NORWAY_TEXTILE_KEYWORD_DOMAIN",
+    "NORWAY_TEXTILE_KEYWORD_SCHEMA_VERSION",
+    "NorwayTextileKeywordQuery",
     "OpportunityCategory",
     "OpportunityDossier",
     "TEXTILE_TAXONOMY_SCHEMA_VERSION",
@@ -40,6 +49,7 @@ __all__ = [
     "UnifiedOpportunityContractError",
     "UnifiedOpportunityContractV1",
     "build_clothing_inventory_queries",
+    "build_norway_textile_keyword_queries",
     "build_opportunity_dossier",
     "build_textile_taxonomy_audit",
     "classify_candidate",
