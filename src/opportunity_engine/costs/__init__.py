0b060641f4874f1868ef62e702fb98954b165855
Hindawi44 <409hindwi@gmail.com>
2026-07-31 13:14:24 +0200

    Export operational landed cost adapter

--- src/opportunity_engine/costs/__init__.py
+++ src/opportunity_engine/costs/__init__.py
@@ -1,5 +1,10 @@
 """Conservative cost contracts for opportunity evaluation."""
 
+from .decision_landed_cost import (
+    build_estimate_from_decision_record,
+    build_operational_landed_cost_export,
+    select_operational_decision,
+)
 from .landed_cost import (
     CostComponentV1,
     LandedCostEstimateError,
@@ -11,5 +16,8 @@ __all__ = [
     "CostComponentV1",
     "LandedCostEstimateError",
     "LandedCostEstimateV1",
+    "build_estimate_from_decision_record",
     "build_landed_cost_snapshot",
+    "build_operational_landed_cost_export",
+    "select_operational_decision",
 ]
