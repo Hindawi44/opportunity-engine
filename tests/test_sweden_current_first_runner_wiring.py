ecd93431b2d9caeceb7c851fb6acb10864cad79b
Hindawi44 <409hindwi@gmail.com>
2026-08-17 15:44:01 +0200

    Remove obsolete Sweden runner wiring test

--- tests/test_sweden_current_first_runner_wiring.py
+++
@@ -1,8 +0,0 @@
-from scripts.run_market_clothing_inventory_discovery import select_market_runner
-
-
-def test_sweden_market_selector_uses_current_first_runner():
-    runner = select_market_runner("SE")
-    assert runner.__module__ == (
-        "scripts.run_sweden_clothing_inventory_discovery_search_current_first"
-    )
